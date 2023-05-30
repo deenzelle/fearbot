befc56f10fda5c1ba0742b300b8c51ad665ab4d0
deenzelle <denlminecraftspam@gmail.com>
2023-05-31 05:16:13 +1200

    Update fearbot.py
    added comments to seperate each section into the parts that I will have to work on throughout these 2 months

--- fearbot.py
+++ fearbot.py
@@ -1,3 +1,25 @@
 # FEARBOT Program
 # A progam that can be used by 'FEARNOT' to order LE SSERAFIM merch
 
+# Create a welcome function that welcomes the customer to the bit
+
+# Asks the user if the order is intended for click and collect or delivery
+
+# Collects the user's name, address and phone number if order is intended for delivery
+
+# Collects the user's name and phone number as well as notifying the user that they will receive a text message if order was intended for click and collect
+
+# Make a menu of at least 12 items for the user to choose from
+# Make each item available item orderable
+# Make the cost of each item display within the menu
+
+# Prints out order details
+# Items & Prices
+# Total cost of order, incl delivery charge (if applicable)
+# Customer's name
+# Customer's phone number
+# (IF ORDER IS FOR DELIVERY) Print out customer's address
+
+# Asks the user if they wish to cancel the order before submitting it
+
+# Ask if user wishes to place another order, or exit the bot
