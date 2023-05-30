f66f294cee9383f1191ca73b616efc9d41f8bb4b
deenzelle <denlminecraftspam@gmail.com>
2023-05-31 11:59:14 +1200

    Update c&c_delivery_menu v1.py

--- components/c&c_delivery_menu v1.py
+++ components/c&c_delivery_menu v1.py
@@ -1,6 +1,9 @@
 # COMPONENT 2
 # VERSION 1
 
+# Bugs
+# Will only work for valid input 'd' and 'p'
+
 print("Do you want your order delivered or are you planning to click & collect?")
 
 print("To select delivery enter 'd', to select Click & Collect enter 'c'")
