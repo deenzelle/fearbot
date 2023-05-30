ca05049575ea19405462f16a93d13378a103d435
deenzelle <denlminecraftspam@gmail.com>
2023-05-31 11:35:17 +1200

    Update fearbot.py
    Integrated welcome and main function (component 1) into the main program (fearbot.py)

--- fearbot.py
+++ fearbot.py
@@ -1,7 +1,37 @@
 # FEARBOT Program
 # A progam that can be used by 'FEARNOT' to order LE SSERAFIM merch
 
-# Create a welcome function that welcomes the customer to the bit
+# Imports a library that allows the program to have randomness where nescessary
+import random
+from random import randint # Imports the ability to choose between integers randomly
+
+# List of names
+names = ["Hanni", "Chaewon", "Nicole", "Kaitlin", "Denzelle", "Harry", "Yeonjun", "Jaehyun", "Jungkook", "Sunghoon"]
+
+# Create a welcome function that welcomes the customer to the bot
+def welcome():
+    '''
+    Purpose: To generate a random name from the list and print out 
+    a welcome message
+    Parameters: None
+    Returns: None 
+    '''
+    # Chooses a random integer between 0 and 9
+    num = randint(0,9)
+    # Sets the name variable equal to the name which corresponds to the above selected integer
+    name = (names[num])
+    # Prints out a welcome banner
+    print("""
+         _____ _____    _    ____  _   _  ___ _____    ____ _____ _   _ _____ ____      _    _     
+        |  ___| ____|  / \  |  _ \| \ | |/ _ \_   _|  / ___| ____| \ | |_   _|  _ \    / \  | |    
+        | |_  |  _|   / _ \ | |_) |  \| | | | || |   | |   |  _| |  \| | | | | |_) |  / _ \ | |    
+        |  _| | |___ / ___ \|  _ <| |\  | |_| || |   | |___| |___| |\  | | | |  _ <  / ___ \| |___ 
+        |_|   |_____/_/   \_\_| \_\_| \_|\___/ |_|    \____|_____|_| \_| |_| |_| \_\/_/   \_\_____|
+        """)
+    # Prints out basic welcome message
+    print("*** Welcome to FEARNOT Central")
+    print("*** My name is" ,name) # Prints out the randomly selected name
+    print("*** I will be to help you order OFFICIAL LE SSERAFIM MERCHANDISE")
 
 # Asks the user if the order is intended for click and collect or delivery
 
@@ -23,3 +53,14 @@
 # Asks the user if they wish to cancel the order before submitting it
 
 # Ask if user wishes to place another order, or exit the bot
+
+# Calls the function/Tells the program to run a specific function
+def main():
+    '''
+    Purpose: To run all functions
+    Parameters: None
+    Returns: None
+    '''
+    welcome()
+    
+main() # Runs the main function
