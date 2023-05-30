0a075976951e947d963b3e2d478aece7f5f73700
deenzelle <denlminecraftspam@gmail.com>
2023-05-31 11:55:54 +1200

    Component 2 V1
    Created a basic menu for selecting between click&collect and delivery. Prints decision depending on what the user has chosen

---
+++ components/c&c_delivery_menu v1.py
@@ -0,0 +1,13 @@
+# COMPONENT 2
+# VERSION 1
+
+print("Do you want your order delivered or are you planning to click & collect?")
+
+print("To select delivery enter 'd', to select Click & Collect enter 'c'")
+
+delivery = input()
+
+if delivery == "d":
+    print ("delivery")
+elif delivery == "c":
+    print("click and collect")
