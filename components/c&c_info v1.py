8aae883360b4dfdf110de632e29d10a4453248a6
deenzelle <denlminecraftspam@gmail.com>
2023-06-01 05:01:31 +1200

    Component 3 V3
    Basic instructions and input for name and phone number
    
    Currently accepts blank input as valid

---
+++ components/c&c_info v1.py
@@ -0,0 +1,11 @@
+# COMPONENT 3
+# VERSION 1
+
+print("Please enter the Click & Collect information")
+# customer name
+name = input("Please enter your name")
+# customer phone number
+phone = input("Please enter your phone number")
+
+print(name)
+print(phone)
