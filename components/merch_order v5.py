c4dbec505b81fb6f4e9fcb5f23a80cd0ba1b8d87
deenzelle <denlminecraftspam@gmail.com>
2023-06-09 16:32:07 +1200

    COMPONENT 6 VERSION 5
    not necessary at this point but good to have. Added code to allow the delivery charge of $9 to be added if the customer orders 5 or less than 5 items

---
+++ components/merch_order v5.py
@@ -0,0 +1,78 @@
+#COMPONENT 6
+# VERSION 5
+# ever so slightly changes
+
+# list of merchandise items available to purchase
+merch_names = ['FEARLESS ALBUM (BLACK PETROL VER)', 'FEARLESS ALBUM (MONOCHROME BOUQUET VER)', 'FEARLESS ALBUM (BLUE CYPHER VER)',
+               'ANTIFRAGILE ALBUM (FROZEN AQUAMARINE VER)', 'ANTIFRAGILE ALBUM (IRIDESCENT OPAL VER)', 'ANTIFRAGILE ALBUM (MIDNIGHT ONYX VER)',
+               'UNFORGIVEN ALBUM (DEWY SAGE VER)', 'UNFORGIVEN ALBUM (DUSTY AMBER VER)', 'UNFORGIVEN ALBUM (BLOODY ROSE VER)', 
+               'LE SSERAFIM LIGHT STICK', 'LE SSERAFIM FLOOR MAT', 'LE SSERAFIM TUMBLER', 'LE SSERAFIM HOODIE']
+
+# list of available merchandise prices
+merch_prices = [69.50, 69.50, 69.50, 69.50, 69.50, 69.50, 69.50, 69.50, 69.50, 45.50, 25.50, 15.50, 45.50]
+
+# list to store ordered merch items
+order_list = []
+# list to store ordered merch prices
+order_cost = []
+
+#Create pizza menu
+def catalog():
+    number_merch = 13
+    for count in range (number_merch):
+        print("{} {} ${:.2f}" .format(count+1,merch_names[count],merch_prices[count]))
+
+def order_merch():
+    # ask for the total amount of merchandise for order
+    num_merch = 0
+
+    # Creates a while true loop to make sure that integer is valid and not a value error
+    # Creates a while true loop to make sure that integer is valid and not a value error
+    while True:
+            try:
+                num_merch = int(input("How many merch pieces would you like to order today?: "))
+                if num_merch >= 1 and num_merch <= 5: 
+                    order_cost.append(9)
+                    break
+                elif num_merch >= 6 and num_merch <= 15:
+                    break
+                else:
+                    print("Please only choose between 1 and 15 merch items to order")
+
+            except ValueError:
+                print("That is not a valid number")
+                print("Please pick between bigger than 1")    
+    print(num_merch)
+    
+    # Choose merch item from menu
+    for item in range(num_merch):
+        while num_merch > 0:
+            while True:
+                try:
+                    merch_ordered = int(input("Please choose your merchandise items by entering the number on the menu: "))
+                    if merch_ordered >= 1 and merch_ordered <= 13:
+                        break
+                    else:
+                        print("Please make sure you choose an available item that is listed above between 1-13")
+                except ValueError:
+                    print("That is not a valid number")
+                    print("Please enter a number between 1 and 13")
+
+            merch_ordered = merch_ordered - 1
+            order_list.append(merch_names[merch_ordered])
+            order_cost.append(merch_prices[merch_ordered])
+            num_merch = num_merch - 1
+            print("{} ${:.2f}" .format(merch_names[merch_ordered],merch_prices[merch_ordered]))
+
+        
+
+# print order
+catalog()
+order_merch()
+
+
+#purely just to check if something works so future me can come back to it
+count = 0
+total_cost = sum(order_cost)
+
+print(f"The total cost: ${total_cost:.2f}")
