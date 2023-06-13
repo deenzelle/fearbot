cec61c9b25873969a9b4bfc29d378ea0479fecc8
deenzelle <denlminecraftspam@gmail.com>
2023-06-14 11:48:14 +1200

    Component 7 V2
    added formatting when printing out user details

---
+++ components/order_print v2.py
@@ -0,0 +1,17 @@
+#Component 7
+#V2
+
+# list to store ordered merch items
+order_list = ['FEARLESS ALBUM (BLACK PETROL VER)', 'FEARLESS ALBUM (MONOCHROME BOUQUET VER)', 'FEARLESS ALBUM (BLUE CYPHER VER)',
+               'ANTIFRAGILE ALBUM (FROZEN AQUAMARINE VER)',]
+# list to store ordered merch prices
+order_cost = [69.50, 69.50, 69.50, 69.50]
+
+cust_details = {'name': 'Denz', 'phone': '0211690403', 'house': '69', 'street': 'Tokki Drive', 'suburb': 'Flat Bush'}
+
+print("\nCustomer Name: {} \nCustomer Phone: {} \nCustomer House Number: {} \nCustomer Street Name: {} \nCustomer Suburb: {}".format(cust_details['name'],cust_details['phone'],cust_details['house'],cust_details['street'],cust_details['suburb'],))
+
+count=0
+for item in order_list:
+    print("Ordered: {}  Cost: ${:.2f}" .format(item, order_cost[count]))
+    count = count + 1
